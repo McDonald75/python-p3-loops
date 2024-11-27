7ffae5d37798c552240e6e47a4884e816a1fcdf9
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-11-27 10:34:31 +0000

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    x = 10
+    while(x > -1):
+        print(x) if x != 0 else print('Happy New Year!')
+        x-=1
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [ints*ints for ints in int_list ]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for number in range(100):
+        number+=1
+        if(number % 3 == 0 and number % 5 == 0):
+            print("FizzBuzz")
+        elif(number % 3 == 0):
+            print('Fizz')
+        elif(number % 5 == 0):
+            print('Buzz')
+        else:
+            print(number)
